41ee355e99beb5ac59cde9badbb2699f3e9bddd1
Kevin Christ <kevin.christ@stud.uni-heidelberg.de>
2024-07-19 11:36:23 +0200

    finished the function. I am now able to press the button and receive 3 random movies

--- index.js
+++ index.js
@@ -1,26 +1,50 @@
 async function fetchData() {
+  const movieElements = [
+    document.getElementById("movie-1"),
+    document.getElementById("movie-2"),
+    document.getElementById("movie-3"),
+  ];
+  const button = document.getElementById("random");
+  const errorMessage = document.getElementById("error");
+
+  const apiKey = "b0a878ff";
   const random = [];
-  const url = `https://www.omdbapi.com/?apikey=b0a878ff&s=movie&type=movie`;
+  const currentYear = new Date().getFullYear();
 
-  try {
-    const response = await fetch(url);
-    if (!response.ok) {
-      throw new Error(`Error: ${response.error}`);
+  for (let i = 0; i < 3; i++) {
+    const randomYear =
+      Math.floor(Math.random() * (currentYear - 1900 + 1)) + 1900;
+    const url =
+      "http://www.omdbapi.com/?apikey=" +
+      apiKey +
+      "&s=movie&y=" +
+      randomYear +
+      "&type=movie";
+    try {
+      const response = await fetch(url);
+      if (!response.ok) {
+        throw new Error(`Error: ${response.error}`);
+      }
+      const data = await response.json();
+      errorMessage.textContent = "";
+      console.log(data);
+
+      if (data.Response === "True" && data.Search && data.Search.length > 0) {
+        const randomMovie =
+          data.Search[Math.floor(Math.random() * data.Search.length)];
+        movieElements[i].innerHTML = `
+          <h3>${randomMovie.Title}</h3>
+          <p>${randomMovie.Year}</p>`;
+      } else {
+        console.log(data.Error);
+        movieElements[i].innerHTML = "<p>Error loading movie</p>";
+      }
+    } catch (error) {
+      console.error(`Error fetching the data: ${error}`);
+      errorMessage.textContent = `Error loading the data`;
+      movieElements[i].innerHTML = "<p>Error loading movie</p>";
     }
-    const data = await response.json();
-
-    console.log(data);
-
-    if (data.Response === "True") {
-    } else {
-      console.log(data.Error);
-    }
-  } catch (error) {
-    console.error(`Error fetching the data: ${error}`);
-    document.getElementById(
-      "results"
-    ).textContent = `<p>Error loading the data</p>`;
   }
 }
 
-fetchData();
+document.getElementById("random").addEventListener("click", fetchData);
